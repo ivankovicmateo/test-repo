2c5e505734205379733e396a33774b14ba5bdbd7
mateo.ivankovic <mateo.ivankovic@king-ict.hr>
2025-02-27 08:17:02 +0100

    ds

--- Electron/AutoUpdate.ts
+++ Electron/AutoUpdate.ts
@@ -1,24 +1,26 @@
-import { dialog, app } from 'electron';
-import { autoUpdater, UpdateInfo } from 'electron-updater';
+import { app, dialog } from "electron";
+import { autoUpdater, UpdateInfo } from "electron-updater";
 
 export default () => {
-    if (app.isPackaged) {
-        autoUpdater.autoInstallOnAppQuit = true;
-        autoUpdater.checkForUpdates();
-        autoUpdater.addListener('update-downloaded', (info: UpdateInfo) => {
-            dialog
-                .showMessageBox({
-                    title: 'Restart Believers Sword?',
-                    type: 'question',
-                    message: `New version "Believers Sword ${info.version}" has been successfully downloaded.`,
-                    buttons: ['Yes', 'Later', 'Yes, Update'],
-                    cancelId: 1,
-                })
-                .then(({ response }) => {
-                    if (response == 0 || response == 2) {
-                        autoUpdater.quitAndInstall();
-                    }
-                });
+  if (app.isPackaged) {
+    autoUpdater.autoInstallOnAppQuit = true;
+    autoUpdater.checkForUpdates();
+    console.log("checked");
+    console.log(autoUpdater.currentVersion);
+    autoUpdater.addListener("update-downloaded", (info: UpdateInfo) => {
+      dialog
+        .showMessageBox({
+          title: "Restart Believers Sword?",
+          type: "question",
+          message: `New version "Believers Sword ${info.version}" has been successfully downloaded.`,
+          buttons: ["Yes", "Later", "Yes, Update"],
+          cancelId: 1,
+        })
+        .then(({ response }) => {
+          if (response == 0 || response == 2) {
+            autoUpdater.quitAndInstall();
+          }
         });
-    }
+    });
+  }
 };
